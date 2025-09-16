67ab7184d222c7ccf4b1aba407aa4dfec50312e2
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2025-09-16 17:32:44 -0400

    Bug fix

--- ps5controllertokeyboard.py
+++ ps5controllertokeyboard.py
@@ -22,45 +22,45 @@ for event in ps5_controller.read_loop():
     if event.type == 1 and event.code == 317:
         break
 
-    if event.type == 3 and event.code == 17 and value == -1:
-        ui.write(e.EV_KEY, e.KEY_W, 1)
+    if event.type == 3 and event.code == 17 and event.value == -1:
+        ui.write(ecodes.EV_KEY, ecodes.KEY_W, 1)
         ui.syn()
         is_key_w_down = True
 
-    if event.type == 3 and event.code == 17 and value == 1:
-        ui.write(e.EV_KEY, e.KEY_S, 1)
+    if event.type == 3 and event.code == 17 and event.value == 1:
+        ui.write(ecodes.EV_KEY, ecodes.KEY_S, 1)
         ui.syn()
         is_key_s_down = True
     
-    if event.type == 3 and event.code == 17 and value == 0:
+    if event.type == 3 and event.code == 17 and event.value == 0:
         if is_key_w_down:
-            ui.write(e.EV_KEY, e.KEY_W, 0)
+            ui.write(ecodes.EV_KEY, ecodes.KEY_W, 0)
             ui.syn()
             is_key_w_down = False
 
         if is_key_s_down:
-            ui.write(e.EV_KEY, e.KEY_S, 0)
+            ui.write(ecodes.EV_KEY, ecodes.KEY_S, 0)
             ui.syn()
             is_key_s_down = False
 
-    if event.type == 3 and event.code == 16 and value == -1:
-        ui.write(e.EV_KEY, e.KEY_A, 1)
+    if event.type == 3 and event.code == 16 and event.value == -1:
+        ui.write(ecodes.EV_KEY, ecodes.KEY_A, 1)
         ui.syn()
         is_key_a_down = True
 
-    if event.type == 3 and event.code == 16 and value == 1:
-        ui.write(e.EV_KEY, e.KEY_D, 1)
+    if event.type == 3 and event.code == 16 and event.value == 1:
+        ui.write(ecodes.EV_KEY, ecodes.KEY_D, 1)
         ui.syn()
         is_key_d_down = True
     
-    if event.type == 3 and event.code == 16 and value == 0:
+    if event.type == 3 and event.code == 16 and event.value == 0:
         if is_key_a_down:
-            ui.write(e.EV_KEY, e.KEY_A, 0)
+            ui.write(ecodes.EV_KEY, ecodes.KEY_A, 0)
             ui.syn()
             is_key_a_down = False
 
         if is_key_d_down:
-            ui.write(e.EV_KEY, e.KEY_D, 0)
+            ui.write(ecodes.EV_KEY, ecodes.KEY_D, 0)
             ui.syn()
             is_key_d_down = False
     
